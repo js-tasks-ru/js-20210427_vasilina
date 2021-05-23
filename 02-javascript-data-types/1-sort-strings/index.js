202ad6f8093c2be78a69343feb7a9637a2dce944
Vasilina <Ignatenko-V@gaz-is.ru>
2021-05-23 21:54:31 +0300

    сортировка 1

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -5,5 +5,15 @@
  * @returns {string[]}
  */
 export function sortStrings(arr, param = 'asc') {
-
+    function compareStringAsc (str1,str2) {
+        return str1.localeCompare(str2, [ 'ru','en' ], { caseFirst: 'upper' });
+    }      
+    function compareStringDesc (str1,str2) {
+        return str2.localeCompare(str1, [ 'ru','en' ], { caseFirst: 'upper' });
+    }
+    if (param =='asc') {
+        return [...arr].sort(compareStringAsc);
+    } else if (param == 'desc') {
+        return [...arr].sort(compareStringDesc);
+    }
 }
